b5ce48a9a4813188a51e59aa681160e0bf1500bf
mayursagar1415 <mayursagar1415@gmail.com>
2024-12-28 20:56:38 +0530

    Delete static directory

--- static/js/dignosis.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('diagnosisForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    // Here you would typically handle the form submission
-    alert('Diagnosis submitted successfully!');
-    this.reset();
-});
